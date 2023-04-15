ac528fe972837e3d76caa806af6c18c0f07572cc
Mateusz <matekumateku@gmail.com>
2023-04-15 12:48:54 +0200

    added script.js and responsive design

---
+++ script.js
@@ -0,0 +1,14 @@
+const bgImage = document.getElementById("bg-image");
+
+window.addEventListener("scroll", () => {
+  updateImage();
+});
+
+function updateImage() {
+  bgImage.style.opacity = 1 - window.pageYOffset / 800;
+  if (window.innerWidth > 900) {
+    bgImage.style.backgroundSize = 160 - window.pageYOffset / 12 + "%";
+  } else {
+    bgImage.style.backgroundSize = 400 - window.pageYOffset / 12 + "%";
+  }
+}
